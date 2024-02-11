3e67ae78447b982a933a5e072e89d11b757c3991
vladcto <55505918+vladcto@users.noreply.github.com>
2024-02-11 18:11:24 +0300

    Теперь suaidoc можно вызывать из любого места

--- suaidoc.sh
+++
@@ -1,9 +0,0 @@
-#! bin/bash
-mkdir $(dirname $0)/tmp
-tmp=$(dirname $0)/tmp
-python3 scripts/update_md.py $1 $tmp/
-python3 scripts/replace.py $1 $tmp/_intro.pdf
-pandoc $tmp/_updated_md.md -o $2 \
-	--template=template.tex --pdf-engine=xelatex \
-	--listings --pdf-engine-opt=-shell-escape
-rm -r $tmp
