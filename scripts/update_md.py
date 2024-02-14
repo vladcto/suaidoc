aec136e94b39c0affd46c2210566b47575b04131
vladcto <55505918+vladcto@users.noreply.github.com>
2024-02-14 03:21:27 +0300

    Добавил теги для формул.

--- scripts/update_md.py
+++ scripts/update_md.py
@@ -50,6 +50,23 @@ def wrap_cyrillic_in_mathit(md_content):
     return latex_formula_pattern.sub(replace_cyrillic_in_formula, md_content)
 
 
+def add_equation_label(md_content):
+    def replace_func(match):
+        equation = match.group(1)
+        sueq_tag = match.group(2)
+        id_match = re.search(r'id="(.*?)"', sueq_tag)
+        if id_match:
+            return f"\\begin{{equation}}\n{equation}\n\\label{{eq:{id_match.group(1)}}}\n\\end{{equation}}"
+        else:
+            return f"\\begin{{equation}}\n{equation}\n\\end{{equation}}"
+
+    # re $$<math>$$\n<sueq*>
+    pattern = r"\$\$(.*?)\$\$\n(<sueq.*?>)"
+    md_content = re.sub(pattern, replace_func, md_content, flags=re.DOTALL)
+
+    return md_content
+
+
 def update_markdown_file(markdown_path, output_file_path):
     markdown_directory = os.path.dirname(os.path.abspath(markdown_path))
     with open(markdown_path, 'r', encoding='utf-8') as file:
@@ -57,5 +74,6 @@ def update_markdown_file(markdown_path, output_file_path):
     md = replace_relative_image_paths(md, markdown_directory)
     md = replace_center_titles(md)
     md = wrap_cyrillic_in_mathit(md)
+    md = add_equation_label(md)
     with open(output_file_path, mode='w+', encoding='utf-8') as output_file:
         output_file.write(md)
