8960ef48d0cbb71a0d222c9fd6c245875a09c408
vladcto <55505918+vladcto@users.noreply.github.com>
2024-02-18 04:46:12 +0300

    Указал зависимости

--- setup.py
+++ setup.py
@@ -5,7 +5,10 @@ setup(
     version='0.0.1',
     py_modules=['suaidoc'],
     install_requires=[
-        'Click',
+        'click',
+        'pdfkit',
+        'python_frontmatter',
+        'setuptools',
     ],
     packages=find_packages(),
     include_package_data=True,
